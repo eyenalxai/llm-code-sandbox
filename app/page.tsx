60b79e9e3a7a759f65cb403a1f2774892d2b69b2
Sax Dhiut <mail@iyxeyl.me>
2024-04-24 20:15:03 +0400

    refactor: remove unused zod schema from page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
 import { cn } from '@/lib/utils'
-import { z } from 'zod'
 import { Editor } from '@/components/editor'
-
-const FormSchema = z.object({
-  code: z
-    .string()
-    .min(8, {
-      message: 'CODE MUST BE AT LEAST 8 CHARACTERS'
-    })
-    .max(4096, {
-      message: 'CODE MUST BE LESS THAN 4096 CHARACTERS'
-    })
-})
 
 export default function Home() {
   return (
